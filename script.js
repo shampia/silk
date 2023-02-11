39aa2ca1b9f1531c896219a54c9b675657daefa0
shampia <118632643+shampia@users.noreply.github.com>
2023-02-11 22:57:36 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.getElementById('services-btn').addEventListener('click',function()
+{
+    document.getElementById('service-page').scrollIntoView({behavior:'smooth'});
+})
+
+document.getElementById('Benefits-btn').addEventListener('click',function()
+{
+    document.getElementById('benefits-page').scrollIntoView({behavior:'smooth'});
+})
+
+document.getElementById('Features-btn').addEventListener('click',function()
+{
+    document.getElementById('feature-page').scrollIntoView({behavior:'smooth'});
+})
+
+document.getElementById('contact-btn').addEventListener('click',function()
+{
+    document.getElementById('contact-page').scrollIntoView({behavior:'smooth'});
+})
